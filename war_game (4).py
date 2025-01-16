aea8813e392df38011e778488a73567c1bf6cff2
Analee Maharaj <120334807+analees@users.noreply.github.com>
2025-01-16 13:33:19 -0600

    Upadted Card class constructor Change Six (4).py
    	    #swapped suit and face
            if (the_suit in suit  or the_face in face):

--- war_game (4).py
+++ war_game (4).py
@@ -17,7 +17,8 @@ class Card:
     # If they are not found in the global variables above, the card will be set to a 2 of clubs
     def __init__(self, the_face, the_suit):
         global face, suit
-        if (the_face in face and the_suit in suit):
+	    #swapped suit and face
+        if (the_suit in suit  or the_face in face):
             self.face = the_face
             self.suit = the_suit
         else:
